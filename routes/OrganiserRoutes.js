13c4be5c098b12b6682a9cac067ee283f22784e2
manishupadhyay911 <upadhyaymanish302@gmail.com>
2024-03-28 19:59:34 +0530

    minor bug fix

--- routes/OrganiserRoutes.js
+++
@@ -1,7 +0,0 @@
-const express= require("express")
-const {createEvent, fetchEvent }  = require("../controllers/eventController")
-const {protect} = require("../middleware/auth");
-const router = express.Router();
-
-router.route('/event').post(protect, createEvent).get(protect, fetchEvent);
-module.exports= router;
